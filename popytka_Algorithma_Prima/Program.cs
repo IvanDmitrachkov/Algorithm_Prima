3c1769b1c35c6256970d94cb292253a87543a70a
IvanDmitrachkov <dmitrachkovivan@gmail.com>
2018-04-06 08:35:40 +0300

    Добавьте файлы проекта.

---
+++ popytka_Algorithma_Prima/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace popytka_Algorithma_Prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
